58f70faaa5a11079d0a9a7f70d5ba9eafada4bbf
Aryan Shah <48070964+ghostarmor@users.noreply.github.com>
2024-08-21 22:03:46 -0400

    temporarily disabled response reviewer agent

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import pandas as pd
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.chat_models import ChatDeepInfra
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, PromptTemplate
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
@@ -34,7 +35,12 @@ all_splits = text_splitter.split_documents(docs)
 print(len(all_splits))
 
 # Initialize embeddings
-embeddings = HuggingFaceEmbeddings()
+model_name = "BAAI/bge-small-en"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": True}
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+)
 
 # Create vector store for YT transcript docs
 yt_transcript_vectorstore = FAISS.from_documents(all_splits, embeddings)
@@ -67,7 +73,7 @@ def get_relevant_qa(query: str):
     # Semantic search
     relevant_docs = vectorstore.similarity_search(query, k=3)
     relevant_questions = [q.page_content for q in relevant_docs]
-    relevant_answers = [answers[questions.index(q)] for q in relevant_questions]
+    #relevant_answers = [answers[questions.index(q)] for q in relevant_questions]
     
     # Keyword search
     tfidf = TfidfVectorizer()
@@ -93,7 +99,7 @@ def get_relevant_docs(query: str, top_k: int):
 
 agent1_prompt_template = ChatPromptTemplate.from_messages([
     SystemMessagePromptTemplate.from_template(
-        "You are an AI assistant specialized in mental health and OCD. Provide a direct, concise answer to the user's question based on the relevant information provided. The context provided will include relevant questions and answers that you can refer to and also relevant mental fitness information you can use when generating your answer. Do not include any analysis steps, thought processes, or explanations of your answer. Just give the final, helpful response."
+        "You are an AI assistant specialized in mental health and OCD. If the question is not related to mental health, say that you cannot answer it. Otherwise, provide a direct, concise answer to the user's question based on the relevant information provided. The context provided will include relevant questions and answers that you can refer to and also relevant mental fitness information you can use when generating your answer. Do not include any analysis steps, thought processes, or explanations of your answer. Just give the final, helpful response and ensure it makes sense in the context of the user's question."
     ),
     ("human", "User question: {user_question}\n\nRelevant general information:\n{context}\n\nRelevant question/answer information:\n{qa_context}\n\nConcise answer:")
 ])
@@ -128,22 +134,22 @@ def ask_question(query: Query):
     final_response = result.content
     metadata1 = result.response_metadata
     
-    review_agent_prompt = reviewer_agent_prompt_template.invoke({"question": query.question, "response": result.content})
-    review = llm.invoke(review_agent_prompt)
-    if review.content.strip().lower() != 'yes':
-        print(f'Reviewer made changes. Original response was {final_response}')
-        final_response = review.content
+    # review_agent_prompt = reviewer_agent_prompt_template.invoke({"question": query.question, "response": result.content})
+    # review = llm.invoke(review_agent_prompt)
+    # if review.content.strip().lower() != 'yes':
+    #     print(f'Reviewer made changes. Original response was {final_response}')
+    #     final_response = review.content
     
-    metadata2 = review.response_metadata
+    # metadata2 = review.response_metadata
     
     return {
         "answer": final_response,
         "relevant_questions": relevant_questions,
         "relevant_answers": relevant_answers,
         "relevant_docs": context,
-        "input_tokens": metadata1['token_usage']['prompt_tokens'] + metadata2['token_usage']['prompt_tokens'],
-        "output_tokens": metadata1['token_usage']['completion_tokens'] + metadata2['token_usage']['completion_tokens'],
-        "estimated_cost": metadata1['token_usage']['estimated_cost'] + metadata2['token_usage']['estimated_cost']
+        "input_tokens": metadata1['token_usage']['prompt_tokens'], #+ metadata2['token_usage']['prompt_tokens'],
+        "output_tokens": metadata1['token_usage']['completion_tokens'], #+ metadata2['token_usage']['completion_tokens'],
+        "estimated_cost": metadata1['token_usage']['estimated_cost'], #+ metadata2['token_usage']['estimated_cost']
     }
 
 if __name__ == "__main__":
